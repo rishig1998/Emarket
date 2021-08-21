e0c8fb64b623e24b67b037be21692187ed1c9ba1
rishikesh gupta <rishig141998@gmail.com>
2021-08-21 08:35:41 +0530

    Add project files.

---
+++ Emarket.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Emarket.Web.Startup))]
+namespace Emarket.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
